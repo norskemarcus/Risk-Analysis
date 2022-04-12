c6f18029448f1b6328042f1dffad16a3c25927af
norskemarcus <marc791j@stud.kea.dk>
2022-04-12 16:17:26 +0200

    made riskanalysis with hardcoded risks. Correct print with colours!

--- src/com/company/RiskAnalysis.java
+++ src/com/company/RiskAnalysis.java
@@ -12,8 +12,39 @@ public class RiskAnalysis {
 
 
 
+  public void displayRiskAnalysis(Risk[] risks) {
+    System.out.println("No.  Risk name \t\t Pro  Con\tSev\t Level");
 
-  public RiskAnalysis(Risk risk){
+    for (int i = 0; i < risks.length; i++) {
+
+      String name = risks[i].getName();
+      int probability = risks[i].getProbability();
+      int consequence = risks[i].getConsequence();
+      int severity = risks[i].getSeverity();
+
+      // # - no. - name - pro - con - sev - level
+      System.out.printf("#%2d %3s\t %d \t %2d \t%2d", i+1, name, probability, consequence, severity);
+      int degreeOfSeverity = risks[i].getSeverity();
+      displaySeverityLevel(degreeOfSeverity);
+      System.out.println("");
+    }
+  }
+
+    public void displaySeverityLevel(int degreeOfSeverity) {
+      if (degreeOfSeverity <= 5) {
+        System.out.print("\t" + ANSI_GREEN + " Very_Low" + ANSI_RESET);
+      } else if (degreeOfSeverity >= 10 && degreeOfSeverity < 15) {
+        System.out.print("\t" + ANSI_GREEN + " Low" + ANSI_RESET);
+      } else if (degreeOfSeverity > 10 && degreeOfSeverity <= 15) {
+        System.out.print("\t" + ANSI_YELLOW + " Medium" + ANSI_RESET);
+      } else if (degreeOfSeverity > 15 && degreeOfSeverity <= 20) {
+        System.out.print("\t" + ANSI_RED + " High" + ANSI_RESET);
+      } else {
+        System.out.print("\t" + ANSI_RED + " Very_High" + ANSI_RESET);
+      }
+
+
+
 
 }
 
--- src/com/company/TestProgram.java
+++ src/com/company/TestProgram.java
@@ -5,9 +5,19 @@ public class TestProgram {
 
   public void mainMenu(){
 
+    // Hardcoded risks
+    Risk risk1 = new Risk("Risk number 1", 1, 1);
+    Risk risk2 = new Risk("Risk number 2", 1, 2);
+    Risk risk3 = new Risk("Risk number 3", 2, 2);
+    Risk risk4 = new Risk("Risk number 4", 3, 2);
+    Risk risk5 = new Risk("Risk number 5", 3, 3);
+    Risk risk6 = new Risk("Risk number 6", 4, 4);
+    Risk risk7 = new Risk("Risk number 7", 4, 5);
+    Risk risk8 = new Risk("Risk number 8", 5, 5);
 
-    Risk risk1 = new Risk("Risk number 0", 1, 1);
+    Risk[] risks = {risk1, risk2, risk3, risk4, risk5, risk6, risk7, risk8};
 
+    new RiskAnalysis().displayRiskAnalysis(risks);
 
   }
 
