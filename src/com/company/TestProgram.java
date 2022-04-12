84fbd40b04392cbe05df0bfa5558e1d905c87782
norskemarcus <marc791j@stud.kea.dk>
2022-04-12 16:37:47 +0200

    fixed displaySeverityLevel for bugs - properly this time

--- src/com/company/TestProgram.java
+++ src/com/company/TestProgram.java
@@ -8,20 +8,24 @@ public class TestProgram {
     // Hardcoded risks
     Risk risk1 = new Risk("Risk number 1", 1, 1);
     Risk risk2 = new Risk("Risk number 2", 1, 2);
-    Risk risk3 = new Risk("Risk number 3", 2, 2);
+    Risk risk3 = new Risk("Risk number 3", 3, 2);
     Risk risk4 = new Risk("Risk number 4", 3, 2);
-    Risk risk5 = new Risk("Risk number 5", 3, 3);
-    Risk risk6 = new Risk("Risk number 6", 4, 4);
-    Risk risk7 = new Risk("Risk number 7", 4, 5);
-    Risk risk8 = new Risk("Risk number 8", 5, 5);
+    Risk risk5 = new Risk("Risk number 5", 3, 4);
+    Risk risk6 = new Risk("Risk number 6", 3, 4);
+    Risk risk7 = new Risk("Risk number 7", 4, 4);
+    Risk risk8 = new Risk("Risk number 8", 4, 4);
+    Risk risk9 = new Risk("Risk number 9", 5, 5);
+    Risk risk10 = new Risk("Risk number 10", 5, 5);
+    Risk risk11 = new Risk("Risk number 11", 5, 5);
+    Risk risk12 = new Risk("Risk number 12", 5, 5);
 
-    Risk[] risks = {risk1, risk2, risk3, risk4, risk5, risk6, risk7, risk8};
+
+    // Add risk into an array:
+    Risk[] risks = {risk1, risk2, risk3, risk4, risk5, risk6, risk7, risk8, risk9, risk10, risk11, risk12};
 
     new RiskAnalysis().displayRiskAnalysis(risks);
 
   }
-
-
 
 
     public static void main(String[] args) {
